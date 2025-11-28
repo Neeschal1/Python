d4230b3fafa2cdea81b2ea61c12a429ae0e7247b
Neeschal1 <neeschalpok04@gmail.com>
2025-11-28 13:19:38 +0545

    array + exception handling

--- exception_handling/03.py
+++ exception_handling/03.py
@@ -1,8 +1,13 @@
-array = [1, 2, "a", 4]
+arr = [1, 2, 3, "a", 4, 5, "rahul", 4.567, False]
+while True:
+    print(arr)
+    user_input = int(input("Enter the index of the array: "))
+    try:
+        result = arr[user_input] / 2
+        print(f"{arr[user_input]} when multiplied by 2 gives: {result} \n")
+    except TypeError:
+        print(f"You can't divide {arr[user_input]} by 2! \n")
+    except IndexError:
+        print(f"The array contains {len(arr)} elements only. Index {user_input} is out of range. I'm sorry! \n")
 
-try:
-    for i in array:
-        # print(array[i])
-        print(array[i] * 5)
-except TypeError:
-    print("Something error occured!")
+
